d5b37c3dbc28326eb7c899d2d9852f59619edc09
BrandonStock1 <46919113@est.ort.edu.ar>
2023-06-27 12:01:44 -0300

    perfil

--- src/components/Usuario.jsx
+++ src/components/Usuario.jsx
@@ -1,105 +1,176 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../scss/layout/_Perfil.scss';
 
-const Usuario = () => {
+function Usuario() {
+  const [formData, setFormData] = useState({
+    nombre: '',
+    apellido: '',
+    edad: '',
+    telefono: '',
+    direccion: '',
+    ano: '',
+    curso: '',
+  });
+  const [userId, setUserId] = useState(null);
+  const [profileData, setProfileData] = useState(null);
+
+  useEffect(() => {
+    // Cargar los datos del usuario existente (si hay un ID de usuario)
+    if (userId) {
+      fetchProfileData(userId);
+    }
+  }, [userId]);
+
+  const handleInputChange = e => {
+    setFormData(prevState => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    try {
+      // Si hay un ID de usuario, realizar una solicitud PUT para actualizar el perfil de usuario
+      if (userId) {
+        const response = await fetch(`http://localhost:3001/api/profiles/${userId}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        });
+
+        if (response.ok) {
+          console.log('Perfil de usuario actualizado correctamente');
+        } else {
+          console.error('Error al actualizar el perfil de usuario');
+        }
+      }
+    } catch (error) {
+      console.error('Error al realizar la solicitud de actualización:', error);
+    }
+  };
+
+  const fetchProfileData = async id => {
+    try {
+      const response = await fetch(`http://localhost:3001/api/profiles/${id}`);
+      if (response.ok) {
+        const data = await response.json();
+        setProfileData(data);
+        setFormData(data);
+      } else {
+        console.error('Error al obtener el perfil de usuario');
+      }
+    } catch (error) {
+      console.error('Error al realizar la solicitud de obtención:', error);
+    }
+  };
+
+  const handleUserIdChange = e => {
+    setUserId(e.target.value);
+  };
+
   return (
     <div className="container pagina">
-        <div className="row">
-          <div className="col-md-3 border-right">
-            <div className="d-flex flex-column align-items-center text-center p-3 py-5">
-              <img className="rounded-circle mt-5" width="150px" src="" alt="" />
-              <span className="font-weight-bold">nombre</span><span className="text-black-50">mail</span><span> </span>
-            </div>
+      <div className="row">
+        <div className="col-md-3 border-right">
+          <div className="d-flex flex-column align-items-center text-center p-3 py-5">
+            <img className="rounded-circle mt-5" width="150px" src="" alt="" />
+            <span className="font-weight-bold">nombre</span>
+            <span className="text-black-50">mail</span>
+            <span> </span>
           </div>
-          <div className="col-md-5 border-right">
-            <div className="p-3 py-5">
+        </div>
+
+        <div className="col-md-5 border-right">
+          <div className="p-3 py-5">
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="userId">ID de Usuario:</label>
+              <input type="number" id="userId" value={userId || ''} onChange={handleUserIdChange} />
               <div className="d-flex justify-content-between align-items-center mb-3">
                 <h4 className="text-right">Perfil</h4>
               </div>
               <div className="row mt-2">
-                <div className="col-md-6">
+              <div className="col-md-12">
                   <label className="labels" style={{ fontSize: '14px' }}>Nombre</label>
-                  <input type="text" className="form-control" placeholder="nombres" value="" />
+                  <input
+                    type="text"
+                    name="nombre"
+                    placeholder="Nombre"
+                    value={formData.nombre}
+                    onChange={handleInputChange}
+                    required
+                  />
                 </div>
-                <div className="col-md-6">
+                <div className="col-md-12">
                   <label className="labels" style={{ fontSize: '14px' }}>Apellido</label>
-                  <input type="text" className="form-control" value="" placeholder="apellido" />
-                </div>
-              </div>
-              <div className="row mt-3">
-                <div className="col-md-12">
-                  <label className="labels" style={{ fontSize: '14px' }}>Número de teléfono</label>
-                  <input type="text" className="form-control" placeholder="" value="" />
-                </div>
-                <div className="col-md-12">
-                  <label className="labels" style={{ fontSize: '14px' }}>Dirección electrónica</label>
-                  <input type="text" className="form-control" placeholder="" value="" />
+                  <input
+                    type="text"
+                    name="apellido"
+                    placeholder="Apellido"
+                    value={formData.apellido}
+                    onChange={handleInputChange}
+                    required
+                  />
                 </div>
                 <div className="col-md-12">
-                  <label className="labels" style={{ fontSize: '14px' }}>Dirección</label>
-                  <input type="text" className="form-control" placeholder="" value="" />
+                  <label className="labels" style={{ fontSize: '14px' }}>Edad</label>
+                  <input
+                    type="number"
+                    name="edad"
+                    placeholder="Edad"
+                    value={formData.edad}
+                    onChange={handleInputChange}
+                    required
+                  />
                 </div>
                 <div className="col-md-12">
-                  <label className="labels" style={{ fontSize: '14px' }}>...</label>
-                  <input type="text" className="form-control" placeholder="" value="" />
+                  <label className="labels" style={{ fontSize: '14px' }}>Direccion</label>
+                  <input
+                    type="text"
+                    name="direccion"
+                    placeholder="Dirección"
+                    value={formData.direccion}
+                    onChange={handleInputChange}
+                    required
+                  />
                 </div>
                 <div className="col-md-12">
-                  <label className="labels" style={{ fontSize: '14px' }}>...</label>
-                  <input type="text" className="form-control" placeholder="" value="" />
+                  <label className="labels" style={{ fontSize: '14px' }}>Año</label>
+                  <input
+                    type="number"
+                    name="ano"
+                    placeholder="Año"
+                    value={formData.ano}
+                    onChange={handleInputChange}
+                    required
+                  />
                 </div>
                 <div className="col-md-12">
-                  <label className="labels" style={{ fontSize: '14px' }}>Habilitados</label>
-                  <input type="text" className="form-control" placeholder="" value="" />
-                </div>
-                <div className="col-md-12">
-                  <label className="labels" style={{ fontSize: '14px' }}>...</label>
-                  <input type="text" className="form-control" placeholder="" value="" />
-                </div>
-                <div className="col-md-12">
-                  <label className="labels" style={{ fontSize: '14px' }}>...</label>
-                  <input type="text" className="form-control" placeholder="" value="" />
-                </div>
-              </div>
-              <div className="row mt-3">
-                <div className="col-md-6">
-                  <label className="labels" style={{ fontSize: '14px' }}>Barrio</label>
-                  <input type="text" className="form-control" placeholder="" value="" />
-                </div>
-                <div className="col-md-6">
-                  <label className="labels" style={{ fontSize: '14px' }}>País</label>
-                  <input type="text" className="form-control" placeholder="" value="" />
+                  <label className="labels" style={{ fontSize: '14px' }}>Curso</label>
+                  <input
+                    type="text"
+                    name="curso"
+                    placeholder="Curso"
+                    value={formData.curso}
+                    onChange={handleInputChange}
+                    required
+                  />
                 </div>
               </div>
               <div className="mt-5 text-center">
-                <button className="btn btn-primary profile-button" type="button">Guardar</button>
+                <button type="submit" className="btn btn-primary profile-button">Guardar Cambios</button>
               </div>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className="p-3 py-5">
-              <div className="d-flex justify-content-between align-items-center experience">
-                <span>Experiencia</span><span className="border px-3 p-1 add-experience"><i className="fa fa-plus"></i>&nbsp;Agregar experiencia</span>
-              </div>
-              <div className="col-md-12">
-                <label className="labels" style={{ fontSize: '14px' }}>Años de experiencia</label>
-                <input type="text" className="form-control" placeholder="" value="" />
-              </div>
-              <div className="col-md-12">
-                <label className="labels" style={{ fontSize: '14px' }}>Posición actual</label>
-                <input type="text" className="form-control" placeholder="" value="" />
-              </div>
-              <div className="col-md-12">
-                <label className="labels" style={{ fontSize: '14px' }}>Descripción</label>
-                <textarea rows="3" className="form-control" placeholder=""></textarea>
-              </div>
-             <div className="mt-5 text-center">
-                <button className="btn btn-primary profile-button" type="button">Guardar</button>
-              </div>
-            </div>
+            </form>
           </div>
         </div>
+      </div>
     </div>
   );
 }
 
 export default Usuario;
+
+
