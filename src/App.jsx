886f4366237985fb35cb2e59e0490f525f7d6f8f
BrandonStock1 <46919113@est.ort.edu.ar>
2023-06-29 15:27:25 -0300

    ENTREGA 1er SPRINT

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
  return (
     <>
        <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Perfil />} />
        </Routes>
     </>
  );
